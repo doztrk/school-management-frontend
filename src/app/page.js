eab2adc7919e32d33087a673ff4801278f71dfe2
Jubaw <doguztrk@gmail.com>
2024-10-19 23:39:48 +0300

    2nd day

--- src/app/page.js
+++ src/app/page.js
@@ -1,56 +1,20 @@
-
+"use client";
+import { Welcome } from "@/components/about/welcome";
+import { Slider } from "@/components/home/slider";
+import { Spacer } from "@/components/common/spacer/spacer";
 import React from "react";
+import { FeaturedCourses } from "@/components/home/featured-courses";
 
 const Page = () => {
 	return (
-		<div>
-		HomePage
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		<p>.</p>
-		</div>
+		<>
+			<Slider />
+			<Spacer />
+			<Welcome />
+			<Spacer />
+			<FeaturedCourses />
+			<Spacer />
+		</>
 	);
 };
 
--- src/components/common/header/main-menubar.jsx
+++ src/components/common/header/main-menubar.jsx
@@ -4,6 +4,8 @@ import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import { Logo } from "./logo";
 import { MainMenu } from "./main-menu";
 import { ButtonCallNow } from "./button-call-now";
+import Image from "next/image";
+import { config } from "@/helpers/config";
 
 export const MainMenuBar = () => {
 	return (
@@ -13,11 +15,17 @@ export const MainMenuBar = () => {
 				<Navbar.Toggle aria-controls="main-menu" />
 				<Navbar.Offcanvas id="main-menu" aria-labelledby="main-menu-title" placement="end">
 					<Offcanvas.Header closeButton>
-						<Offcanvas.Title id="main-menu-title">Offcanvas</Offcanvas.Title>
+						<Offcanvas.Title id="main-menu-title">
+							<Image src="/img/logos/logo-one-line.png"
+							width={253} 
+							height={29}
+							alt={config.project.name} 
+							/>
+						</Offcanvas.Title>
 					</Offcanvas.Header>
 					<Offcanvas.Body>
 						<MainMenu className="justify-content-center flex-grow-1" />
-                        <ButtonCallNow/>
+						<ButtonCallNow />
 					</Offcanvas.Body>
 				</Navbar.Offcanvas>
 			</Container>
